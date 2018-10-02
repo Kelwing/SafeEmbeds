d5ca95766300947c6088f973256cfe10c4a79982
callidus <bart1evers@gmail.com>
2018-10-02 20:57:23 +0200

    adding 400 safety for set_author and set_footer

--- safeembeds/__init__.py
+++ safeembeds/__init__.py
@@ -1,6 +1,8 @@
 import discord
-from .embed import new_add_field
+from .embed import new_add_field, new_set_footer, new_set_author
 
 
 def patch_discord():
     discord.Embed.add_field = new_add_field
+    discord.Embed.set_footer = new_set_footer
+    discord.Embed.set_author = new_set_author
